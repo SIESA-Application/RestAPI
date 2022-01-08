ef416340dbb2dd2bf4bea08c1cd5dda0a31897fa
Diego Herrera <daherre0@gmail.com>
2022-01-08 18:06:05 -0500

    Se agregan los métodos http para el CRUD REST en las rutas y se pprueba con un Hello World en el navegador y con la extensión de VSCode, REST Client.

--- routes/mascotas.js
+++ routes/mascotas.js
@@ -2,4 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 
+//We want to create some routes for:
+
+// Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Getting one
+router.get('/:id', (req, res) => {
+
+});
+
+// Creating one
+router.post('/', (req, res) => {
+
+});
+
+// Updating one (We use patch instead of put, because we want to update only the field the user wants to update, not the whole entry.)
+router.patch('/:id', (req, res) => {
+
+});
+
+// Deleting one
+router.delete('/:id', (req, res) => {
+
+});
+
+
+
+
+
 module.exports = router;
